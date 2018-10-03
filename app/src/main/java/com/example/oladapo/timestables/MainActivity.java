f7739154d9fd543de00230a8b8e9e80af4440bb9
Anthony <you@example.comcls>
2018-10-03 19:16:24 +0000

    added code for seekbar

--- app/src/main/java/com/example/oladapo/timestables/MainActivity.java
+++ app/src/main/java/com/example/oladapo/timestables/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.oladapo.timestables;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timesTableSeekBar = findViewById(R.id.timesTableSeekBar);
+        ListView timesTableListView = findViewById(R.id.timesTableListView);
+
+        timesTableSeekBar.setMax(20);
+        timesTableSeekBar.setProgress(10);
+
+        timesTableSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                int min =1;
+                int timesTable;
+
+                if (progress < min) {
+
+                    timesTable = min;
+
+                } else {
+
+                    timesTable = progress;
+
+                }
+
+                Log.i("seekbar value ", Integer.toString(timesTable));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 }
