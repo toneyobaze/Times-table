cf791985d1184267b83952f649a1173519fa4d48
Anthony <you@example.comcls>
2018-10-03 19:36:48 +0000

    minor changes

--- app/src/main/java/com/example/oladapo/timestables/MainActivity.java
+++ app/src/main/java/com/example/oladapo/timestables/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.oladapo.timestables;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SeekBar;
@@ -11,13 +10,31 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView timesTableListView;
+
+    public void generateTimesTable(int timesTable) {
+
+        ArrayList<String> timesTableContent = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+
+            timesTableContent.add(Integer.toString(i * timesTable));
+
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, timesTableContent);
+
+        timesTableListView.setAdapter(arrayAdapter);
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         final SeekBar timesTableSeekBar = findViewById(R.id.timesTableSeekBar);
-        ListView timesTableListView = findViewById(R.id.timesTableListView);
+        timesTableListView = findViewById(R.id.timesTableListView);
 
         timesTableSeekBar.setMax(20);
         timesTableSeekBar.setProgress(10);
@@ -40,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
                 }
 
-                Log.i("seekbar value ", Integer.toString(timesTable));
+                generateTimesTable(timesTable);
 
             }
 
@@ -55,18 +72,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        int timesTable = 10;
+        generateTimesTable(10);
 
-        ArrayList<String> timesTableContent = new ArrayList<>();
-
-        for (int i = 1; i <= 10; i++) {
-
-            timesTableContent.add(Integer.toString(i * timesTable));
-
-        }
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, timesTableContent);
-
-        timesTableListView.setAdapter(arrayAdapter);
     }
 }
